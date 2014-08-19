9029fe43ed70b56a8d70a653b8685996538e33c7
Joan Llobera <joan.llobera@epfl.ch>
2014-08-19 22:52:21 +0200

    the files needed for the timepath4unity demo project

---
+++ Assets/AdditionalMaterials/TriggerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using timepath4unity;
+
+public class TriggerSpawner : MonoBehaviour {
+	
+
+	bool once=true;
+	Spawner s;
+	// Use this for initialization
+	void Start () {
+		s =(Spawner) this.GetComponent (typeof(Spawner));
+		s.BeginSpawning ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (once && s.transform.GetComponentsInChildren<Transform> ().Length >= s.SpawnCount) {
+			Debug.Log ("we have spawned the small cans");			
+			TPAgent[] agents = Resources.FindObjectsOfTypeAll<TPAgent>();
+			foreach(TPAgent a in agents)
+				a.gameObject.SetActive(true);
+			once = false;
+		}
+	}
+}
