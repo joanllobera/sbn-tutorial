b311778048ef77b3265ed94e25edc71992f7a5a9
Joan Llobera <joan.llobera@epfl.ch>
2014-08-29 16:40:29 +0200

    first case of compiled Dll that does the callbacks

--- Assets/AdditionalMaterials/TriggerSpawner.cs
+++ Assets/AdditionalMaterials/TriggerSpawner.cs
@@ -18,13 +18,13 @@ public class TriggerSpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
+        /*
 		if (once && s.transform.GetComponentsInChildren<Transform> ().Length >= s.SpawnCount) {
 			Debug.Log ("we have spawned the small cans");			
 			TPAgent[] agents = Resources.FindObjectsOfTypeAll<TPAgent>();
 			foreach(TPAgent a in agents)
 				a.gameObject.SetActive(true);
 			once = false;
-		}
+		}*/
 	}
 }
