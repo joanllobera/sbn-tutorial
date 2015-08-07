8d00503bc5992c86b6a42d02c7438f3fc2824420
Joan Llobera <joan.llobera@epfl.ch>
2015-08-07 15:35:12 +0200

    a first version of the timepath4unity tutorial with a character moving things

---
+++ Assets/AdditionalMaterials/Meteorite.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteorite : MonoBehaviour {
+
+	private float startTime;
+	public float lifeTime;
+	public float secondsAlive;
+	
+    private Vector3 initialScale;
+
+	// Use this for initialization
+	void Start () 
+	{
+        initialScale = transform.localScale;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        //Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+       
+		secondsAlive = Time.time - startTime;
+
+
+		if(  secondsAlive >= lifeTime|| (transform.position.y < -2 ) )
+		{
+            MeteoriteSpawner m= transform.parent.GetComponent<MeteoriteSpawner>();
+            m.oneLess();
+			DestroyImmediate (gameObject);
+            
+		}
+
+
+	}
+
+
+    void OnCollisionEnter(Collision collision) {
+
+        if( gameObject.tag.Equals("falling"))
+        {
+            gameObject.tag = "pickme";
+        }
+
+        if (collision.gameObject.tag.Equals("blueRamp"))
+        {
+            gameObject.tag = "blueRamp";
+        }
+        else if (collision.gameObject.tag.Equals("redRamp"))
+        {
+            gameObject.tag = "redRamp";
+        }
+
+        
+    }
+
+
+}
---
+++ Assets/timepath4unity/TPMentalBag.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*!
+\brief
+A handy component usefull to keep track of elements involved in the ongoing actions.
+ A cognitive scientist would call this the working memory
+ A Game AI programmer would call it a billboard  
+ */ 
+
+public class TPMentalBag : MonoBehaviour {
+
+    public Meteorite M;
+    public string destinationTag;
+
+    protected float onDestination = 0.0f;
+
+    public float OnDestination{get{return onDestination;}}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+    
+    void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("the tag I am in is:" + collision.collider.tag);
+
+        if (gameObject.tag.Equals(destinationTag))
+        {
+            onDestination = 1.0f;
+        }
+        else
+        {
+            onDestination = 0.0f;
+
+        }
+    
+
+    }
+
+
+
+    void OnTriggerEnter(Collider c)
+    {
+
+        Debug.Log("the trigger is from object :" + c.tag);
+
+        if (c.tag.Equals(destinationTag))
+        {
+            onDestination = 1.0f;
+        }
+        else
+        {
+            onDestination = 0.0f;
+
+        }
+
+
+    }*/
+
+
+
+
+
+}
